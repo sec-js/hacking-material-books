cb18795761b405795db17134b3e9653dc087d062
pedro ubuntu <r00t-3xp10it@users.noreply.github.com>
2026-05-02 11:41:59 +0100

    Enhance install_nmap_modules.ps1 with mode and error handling
    Added a mode parameter to specify installation or update of modules. Enhanced error handling for Nmap installation path and added messages for successful operations.

--- nmap-NSE/install_nmap_modules.ps1
+++ nmap-NSE/install_nmap_modules.ps1
@@ -4,11 +4,12 @@
    Helper - install vulners.nse + AXISwebcam-enum.nse into nmap database
 
 .NOTES
-   Administrator privileges required to install modules
+   Administrator privileges required to install\update modules
 #>
 
 [CmdletBinding(PositionalBinding=$false)] param(
-   [string]$NmapInstallPath="C:\Program Files (x86)\Nmap"
+   [string]$NmapInstallPath="C:\Program Files (x86)\Nmap",
+   [string]$Mode="install"
 )
 
 $ErrorActionPreference = "SilentlyContinue"
@@ -18,75 +19,94 @@ echo ""
 ## check for admin privileges
 If([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544") -match "false")
 {
-   Write-Host "[ABORT]: administrator privileges required to install nse modules.." -ForegroundColor Red
+   Write-Host "[ABORT]: administrator privileges required to install nse modules..`n" -ForegroundColor Red
    return
 }
 
-## Vulners
-$SuccessfulyInstalledModuleVulners = "false"
-if(Test-path -path "$NmapInstallPath\scripts\vulners.nse" -PathType Leaf)
+## check nmap install directory
+If(-not(Test-Path -Path "$NmapInstallPath"))
 {
-   write-host "[ABORT]: " -NoNewline
-   write-host "$NmapInstallPath\scripts\vulners.nse" -ForegroundColor Red -NoNewline
-   write-host " already installed"
+   Write-Host "[ABORT]: nmap directory not found in: $NmapInstallPath" -ForegroundColor Red
+   Write-Host "Input nmap directory: " -ForegroundColor Red -NoNewline
+   $NmapInstallPath = Read-Host
 }
-else
+
+## Install modules
+If($Mode -imatch '^(install)$')
+{
+   ## install Vulners.nse
+   if (Test-path -path "$NmapInstallPath\scripts\vulners.nse" -PathType Leaf)
+   {
+      write-host "[ABORT]: " -NoNewline
+      write-host "$NmapInstallPath\scripts\vulners.nse" -ForegroundColor Red -NoNewline
+      write-host " already installed"
+   }
+   else
+   {
+      Write-Host "[*] downloading: vulners.nse"
+      iwr -Uri "https://raw.githubusercontent.com/r00t-3xp10it/hacking-material-books/refs/heads/master/nmap-NSE/vulners.nse" -OutFile "$Env:TMP\vulners.nse"|Unblock-File
+      Write-Host "[*] move-item: vulners.nse to $NmapInstallPath\scripts\vulners.nse"
+      Move-Item -Path "$Env:TMP\vulners.nse" -Destination "$NmapInstallPath\scripts\vulners.nse" -Force
+
+      if (Test-path -path "$NmapInstallPath\scripts\vulners.nse" -PathType Leaf)
+      {
+         Write-Host "[*] moved vulners.nse to nmap scripts directory" -ForegroundColor Green
+         Write-Host "[+] updating nmap nse database with vulners.nse" -ForegroundColor Green
+         nmap.exe --script-updatedb
+      }
+      else
+      {
+         Write-Host "[-] ERROR: moving vulners.nse to nmap scripts directory" -ForegroundColor Red
+      }
+   }
+
+   ## install AXISwebcam-enum.nse
+   if (Test-path -path "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -PathType Leaf)
+   {
+      write-host "[ABORT]: " -NoNewline
+      write-host "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -ForegroundColor Red -NoNewline
+      write-host " already installed"
+   }
+   Else
+   {
+      Write-Host "[*] downloading: AXISwebcam-enum.nse"
+      iwr -Uri "https://raw.githubusercontent.com/r00t-3xp10it/hacking-material-books/refs/heads/master/nmap-NSE/AXISwebcam-enum.nse" -OutFile "$Env:TMP\AXISwebcam-enum.nse"|Unblock-File
+      Write-Host "[*] move-item: AXISwebcam-enum.nse to $NmapInstallPath\scripts\AXISwebcam-enum.nse"
+      Move-Item -Path "$Env:TMP\AXISwebcam-enum.nse" -Destination "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -Force
+
+      if (Test-path -path "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -PathType Leaf)
+      {
+         Write-Host "[*] moved AXISwebcam-enum.nse to nmap scripts directory" -ForegroundColor Green
+         Write-Host "[+] updating nmap nse database with AXISwebcam-enum.nse" -ForegroundColor Green
+         nmap.exe --script-updatedb
+      }
+      Else
+      {
+         Write-Host "[-] ERROR: moving AXISwebcam-enum.nse to nmap scripts directory" -ForegroundColor Red
+      }
+   }
+}
+
+
+## update modules
+If($Mode -imatch '^(update)$')
 {
    Write-Host "[*] downloading: vulners.nse"
    iwr -Uri "https://raw.githubusercontent.com/r00t-3xp10it/hacking-material-books/refs/heads/master/nmap-NSE/vulners.nse" -OutFile "$Env:TMP\vulners.nse"|Unblock-File
    Write-Host "[*] move-item: vulners.nse to $NmapInstallPath\scripts\vulners.nse"
    Move-Item -Path "$Env:TMP\vulners.nse" -Destination "$NmapInstallPath\scripts\vulners.nse" -Force
 
-   if(Test-path -path "$NmapInstallPath\scripts\vulners.nse" -PathType Leaf)
-   {
-      $SuccessfulyInstalledModuleVulners = "true"
-   }
-   else
-   {
-      Write-Host "[-] ERROR: installing vulners.nse" -ForegroundColor Red
-      $SuccessfulyInstalledModuleVulners = "false"
-   }
-}
-
-## AXIS
-$SuccessfulyInstalledModuleAxis = "false"
-if(Test-path -path "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -PathType Leaf)
-{
-   write-host "[ABORT]: " -NoNewline
-   write-host "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -ForegroundColor Red -NoNewline
-   write-host " already installed"
-}
-else
-{
    Write-Host "[*] downloading: AXISwebcam-enum.nse"
    iwr -Uri "https://raw.githubusercontent.com/r00t-3xp10it/hacking-material-books/refs/heads/master/nmap-NSE/AXISwebcam-enum.nse" -OutFile "$Env:TMP\AXISwebcam-enum.nse"|Unblock-File
    Write-Host "[*] move-item: AXISwebcam-enum.nse to $NmapInstallPath\scripts\AXISwebcam-enum.nse"
    Move-Item -Path "$Env:TMP\AXISwebcam-enum.nse" -Destination "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -Force
 
-   if(Test-path -path "$NmapInstallPath\scripts\AXISwebcam-enum.nse" -PathType Leaf)
-   {
-      $SuccessfulyInstalledModuleAxis = "true"
-   }
-   else
-   {
-      Write-Host "[-] ERROR: installing AXISwebcam-enum.nse" -ForegroundColor Red
-      $SuccessfulyInstalledModuleAxis = "false"
-   }
-}
-
-## update nse database
-If($SuccessfulyInstalledModuleVulners -match "true")
-{
    nmap.exe --script-updatedb
-   Write-Host "[+] Vulners.nse succeffuly installed" -ForegroundColor Green
-}
-If($SuccessfulyInstalledModuleAxis -match "true")
-{
-   nmap.exe --script-updatedb
-   Write-Host "[+] AXISwebcam-enum.nse succeffuly installed" -ForegroundColor Green
+   Write-Host "[+] nmap nse database updated with downloaded scripts" -ForegroundColor Green
 }
 
 ## cleanup
+Write-Host "[+] cleanup artifacts left behind by this script" -ForegroundColor Green
 Remove-Item -Path "$Env:TMP\vulners.nse" -Force
 Remove-Item -Path "$Env:TMP\AXISwebcam-enum.nse" -Force
 
